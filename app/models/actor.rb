408170f752aa87c97df7c4a338c3fcf21fc3a308
Sarah <sarahlchavez@gmail.com>
2021-05-06 10:05:39 -0700

    commit tree c2dbe4773e8062e6f1c1b358b714fa38da906484

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        actor_name = "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        #list actor's characters and list shows of those characters
+        self.characters.map do |character|
+            "#{character.name} - #{character.show.name}"
+        end
+    end
 end
+
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+    def actors_list
+      # array of actor full name
+      # associated with a show
+      # remember a show has many actors through characters
+      #  "#{Actor.first_name} #{Actor.last_name}
+      # 
+      
+      self.actors.map do |actor|
+        "#{actor.first_name} #{actor.last_name}"
+      end
+    end
+
 end
